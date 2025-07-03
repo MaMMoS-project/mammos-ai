87bb63cd6b8e37f29260d72ad5e11abd48546e7a
Martin Lang <martin.lang@mpsd.mpg.de>
2025-07-03 09:20:51 +0200

    Adapt tests to refactored mammos-entity

--- tests/test_hysteresis.py
+++ tests/test_hysteresis.py
@@ -10,9 +10,9 @@ def test_Hc_Mr_BHmax_from_Ms_A_K():
     Ku = me.Ku(1e6)
 
     extrinsic_properties = mammos_ai.Hc_Mr_BHmax_from_Ms_A_K(Ms, A, Ku)
-    assert extrinsic_properties.Hc > 0
-    assert extrinsic_properties.Mr > 0
-    assert extrinsic_properties.BHmax > 0
+    assert extrinsic_properties.Hc.q > 0
+    assert extrinsic_properties.Mr.q > 0
+    assert extrinsic_properties.BHmax.q > 0
 
 
 def test_Hc_Mr_BHmax_from_Ms_A_K_wrong_model():
