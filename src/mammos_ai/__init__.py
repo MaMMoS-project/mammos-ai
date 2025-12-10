453bd4d5c004d82e6815d388c16addbdb609e0fe
Sam Holt <48217392+samjrholt@users.noreply.github.com>
2025-12-10 17:45:32 +0100

    Add First model from MPCDF (intrinsic to extrinsic predictions)
    Co-authored-by: Martin Lang <67915889+lang-m@users.noreply.github.com>

--- src/mammos_ai/__init__.py
+++ src/mammos_ai/__init__.py
@@ -2,8 +2,11 @@
 
 import importlib.metadata
 
-from ._hysteresis import Hc_Mr_BHmax_from_Ms_A_K
+from ._beyond_stoner_wohlfarth_fixed_angle import (
+    Hc_Mr_BHmax_from_Ms_A_K,
+    is_hard_magnet_from_Ms_A_K,
+)
 
 __version__ = importlib.metadata.version(__package__)
 
-__all__ = ["Hc_Mr_BHmax_from_Ms_A_K"]
+__all__ = ["Hc_Mr_BHmax_from_Ms_A_K", "is_hard_magnet_from_Ms_A_K"]
