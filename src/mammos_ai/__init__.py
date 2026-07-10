b6e26b88839f53c73f46584494839c5cdccbc3c2
AdamsMP92 <adamsmp1992@gmail.com>
2026-07-10 16:35:45 +0200

    added method entry in __init__.py

--- src/mammos_ai/__init__.py
+++ src/mammos_ai/__init__.py
@@ -5,6 +5,7 @@ import importlib.metadata
 from ._beyond_stoner_wohlfarth_fixed_angle import (
     Hc_Mr_BHmax_from_Ms_A_K,
     Hc_Mr_BHmax_from_Ms_A_K_metadata,
+    Ms_A_K_from_Hc_Mr_BHmax,
     is_hard_magnet_from_Ms_A_K,
     is_hard_magnet_from_Ms_A_K_metadata,
 )
@@ -14,6 +15,7 @@ __version__ = importlib.metadata.version(__package__)
 __all__ = [
     "Hc_Mr_BHmax_from_Ms_A_K",
     "Hc_Mr_BHmax_from_Ms_A_K_metadata",
+    "Ms_A_K_from_Hc_Mr_BHmax",
     "is_hard_magnet_from_Ms_A_K",
     "is_hard_magnet_from_Ms_A_K_metadata",
 ]
